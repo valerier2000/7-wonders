16f1a5bc5c78d35d3d57fe6eae3f52ac196a36e2
Valeriia Rohatynska <valeriiarohatynska@gmail.com>
2024-10-19 18:22:33 +0200

    Add MIN_PLAYERS const to Main

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,23 +1,24 @@
 import { useState } from "react";
 import WonderTable from "./WonderTable/WonderTable";
 import PlayerSelect from "./PlayerSelect";
+import { MIN_PLAYERS } from "../utils/constants";
 
 function Main() {
   const [isCalculated, setIsCalculated] = useState(false);
-  const [numPlayers, setNumPlayers] = useState(3);
+  const [numPlayers, setNumPlayers] = useState(MIN_PLAYERS);
 
   function calculate() {
-    setIsCalculated((prevIsCalculated) => {
-      console.log("calculated");
-      return true;
-    });
+    // setIsCalculated((prevIsCalculated) => {
+    console.log("calculated");
+    //   return true;
+    // });
   }
 
   function clear() {
-    setIsCalculated((prevIsCalculated) => {
-      console.log("cleared");
-      return false;
-    });
+    // setIsCalculated((prevIsCalculated) => {
+    console.log("cleared");
+    //   return false;
+    // });
   }
 
   return (
@@ -25,6 +26,7 @@ function Main() {
       <PlayerSelect numPlayers={numPlayers} setNumPlayers={setNumPlayers} />
 
       <WonderTable isCalculated={isCalculated} numPlayers={numPlayers} />
+
       <div className="buttons">
         <button onClick={calculate}>Who won?</button>
         <button onClick={clear}>Clear</button>
