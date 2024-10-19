11768f5047c1fe283992daffc8ceebb3e7c78212
Valeriia Rohatynska <valeriiarohatynska@gmail.com>
2024-10-19 16:54:50 +0200

    Refactor: improve naming in PlayerSelect

--- src/components/PlayerSelect.js
+++ src/components/PlayerSelect.js
@@ -2,20 +2,20 @@ import React from "react";
 import { MIN_PLAYERS, MAX_PLAYERS } from "./../utils/constants";
 
 function PlayerSelect({ numPlayers, setNumPlayers }) {
-  const playersNumber = Array.from(
+  const playerNumOptions = Array.from(
     { length: MAX_PLAYERS - MIN_PLAYERS + 1 },
     (_, i) => MIN_PLAYERS + i
   );
 
-  function handleChange(e) {
+  function handleNumPlayersChange(e) {
     setNumPlayers(e.target.value);
   }
 
   return (
     <div>
       <label htmlFor="players">Select number of players: </label>
-      <select id="players" value={numPlayers} onChange={handleChange}>
-        {playersNumber.map((number) => (
+      <select id="players" value={numPlayers} onChange={handleNumPlayersChange}>
+        {playerNumOptions.map((number) => (
           <option key={number} value={number}>
             {number}
           </option>
